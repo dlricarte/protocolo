c3465cc35c89fd3d0913002e44b3d970c838c382
Renan Jhonatha (^-^) <renan@proesc.com>
2014-06-28 02:05:04 -0300

    Alteração da estrutura do index

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
-<?php
-
+<!DOCTYPE html>
+<!--
 #AUTOR: RENAN JHONATHA
 #DATA: 28 DE JUNHO DE 2014
 #FUNÇÃO: Aplicação e desenvolvimento de envio do fluxo de documentos e exames de uma portaria
 #EQUIPE: DESENVOLVIMENTO DO PRODÍGIO - SISTEMA DE LAUDO ELETRONICO
-
-<center>Laudo Eletronico</center>
+-->
+<html lang="pt-br">
+	<title>Laudo Eletrônico</title>
+	<head>
+		<meta charset="UTF-8">	
+	    <link rel="shortcut icon" href="imagens/favicon.png"/>
+	</head>
+		<body>
+			<center><h3>PRODÍGIO</h3></center>
+			<hr>	
+				<table width=20% align="center" border="1">
+						<tr>
+							<td width="10">Entrada de Documento</td>
+							<td width="10"><a href=""><img src="imagens/selecionar.png"></a></td>
+						</tr>
+						<tr>
+							<td width="10">Entrada de Exame</td>
+							<td width="10"><a href=""><img src="imagens/selecionar.png"></a></td>
+						</tr>
+				</table>
+			<center><br><font size=2>Desenvolvido por ®<br>
+			Desenvolvimento do PRODÍGIO - Soluções Inteligentes
+			</font></center>
+		</body>
+</html>
