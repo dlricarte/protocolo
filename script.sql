838120b04e89a55588cefb04a283dc2a21dd1d87
Renan Jhonatha (^-^) <renan@proesc.com>
2014-06-28 02:05:16 -0300

    Criação da estruturação e modelagem do banco de dados

---
+++ script.sql
@@ -0,0 +1,29 @@
+/*
+===============================================================
+||     							     ||
+||      CRIANDO O BANCO DE DADOS DA APLIAÇÃO   		     ||
+||						             ||
+==============================================================
+*/
+
+--TABELA DE FLUXOS
+
+CREATE TABLE fluxos (
+
+
+);
+
+
+--TABELA DE SETORES
+
+CREATE TABLE setores (
+
+
+);
+
+--TABELA DE PROCESSOS
+
+CREATE TABLE processos (
+
+
+);
