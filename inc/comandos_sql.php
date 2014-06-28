b08277fe03376ee716b5dff6b6a568b185b02ddb
Renan Jhonatha (^-^) <renan@proesc.com>
2014-06-28 03:07:10 -0300

    Arquivos adicionas

--- inc/comandos_sql.php
+++ inc/comandos_sql.php
@@ -1,1 +1,18 @@
 <?php
+
+
+echo $processo =  $_POST[processo];
+
+
+
+if ($processo == "PEE"){
+
+	echo "<br>Voce escolheu um Protocolo de Exame";
+
+}
+
+else if($processo == "PED"){
+
+	echo "<br>Voce escolheu um Protocolo de Documento";
+
+}
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 #FUNÇÃO: Aplicação e desenvolvimento de envio do fluxo de documentos e exames de uma portaria
 #EQUIPE: DESENVOLVIMENTO DO PRODÍGIO - SISTEMA DE LAUDO ELETRONICO
 -->
+
 <html lang="pt-br">
 	<title>Laudo Eletrônico</title>
 	<head>
@@ -13,19 +14,43 @@
 	</head>
 		<body>
 			<center><h3>PRODÍGIO</h3></center>
-			<hr>	
-				<table width=20% align="center" border="1">
+			<hr>
+			<!-- Formulário de envio com os dados-->
+			<form name="entrada" action="inc/comandos_sql.php" method="POST">
+				<!-- Tabela de opções da portaria-->
+				<table width=22% align="center" border="0">
 						<tr>
-							<td width="10">Entrada de Documento</td>
-							<td width="10"><a href=""><img src="imagens/selecionar.png"></a></td>
+							<td width="4">
+							<input type="radio" name="processo" value="PED">	
+							</td>
+							<td width="4"><center><img type="submit" src="imagens/selecionar.png" title="Clique para selecionar uma Entrada de Docoumento"></center></td>
+							<td width="15">Entrada de Documento</td>
 						</tr>
 						<tr>
-							<td width="10">Entrada de Exame</td>
-							<td width="10"><a href=""><img src="imagens/selecionar.png"></a></td>
+							<td width="4">
+							<input type="radio" name="processo" value="PEE">
+							<td width="4"><center><img type="submit" src="imagens/selecionar.png" title="Clique para selecionar uma Entrada de Exame"></center></td>
+							<td width="15">Entrada de Exame</td>
 						</tr>
-				</table>
+						<tr>
+							<td colspan="3"><p><br/></p>								
+								<center><input type="submit" name="criar" value="Abrir Solicitação" title="Clique aqui para Abrir a solicitação"></center>	
+							</td>
+						</tr>
+				</table>				
+				</form>
+
+				<p><br/></p>
+				<p><br/></p>
+			<!-- Rodapé da página-->
 			<center><br><font size=2>Desenvolvido por ®<br>
 			Desenvolvimento do PRODÍGIO - Soluções Inteligentes
 			</font></center>
 		</body>
 </html>
+<script>
+
+if (form.entrada.processo.value = ""){
+ alert ();
+}
+</script>
