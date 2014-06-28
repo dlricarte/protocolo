a15e4b5584cb78ffec42031e7fef315e32fb8252
Renan Jhonatha (^-^) <renan@proesc.com>
2014-06-28 01:13:01 -0300

    Criado o arquivo index.php pra o inicio de criação e testes da aplicação

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+#AUTOR: RENAN JHONATHA
+#DATA: 28 DE JUNHO DE 2014
+#FUNÇÃO: Aplicação e desenvolvimento de envio do fluxo de documentos e exames de uma portaria
+#EQUIPE: DESENVOLVIMENTO DO PRODÍGIO - SISTEMA DE LAUDO ELETRONICO
+
+<center>Laudo Eletronico</center>
